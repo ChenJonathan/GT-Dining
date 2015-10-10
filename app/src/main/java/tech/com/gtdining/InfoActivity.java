f8b005057ba3721832e4e6820b982e3d22d47c1e
kevin-tian <kevin_tian@outlook.com>
2015-10-10 18:20:58 -0400

    Added code to change button color

--- app/src/main/java/tech/com/gtdining/InfoActivity.java
+++ app/src/main/java/tech/com/gtdining/InfoActivity.java
@@ -27,6 +27,7 @@ public class InfoActivity extends Activity {
         //menu = getMenu(diningHall, day);
     }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
