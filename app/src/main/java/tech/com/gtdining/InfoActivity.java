d0044ab05a24f6267a0963a5693115d5c92464e7
Patriones <patriones@gmail.com>
2015-10-10 18:09:29 -0400

    Update InfoActivity.java

--- app/src/main/java/tech/com/gtdining/InfoActivity.java
+++ app/src/main/java/tech/com/gtdining/InfoActivity.java
@@ -8,6 +8,13 @@ import android.view.MenuItem;
 import android.view.View;
 
 import java.util.Calendar;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.*;
 
 public class InfoActivity extends Activity implements View.OnClickListener {
 
